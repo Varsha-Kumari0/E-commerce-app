d8fd40c820d11e2bee249320d3660f7d3f975f1b
Hardin <subhagyachhabra18718@gmail.com>
2024-05-13 00:57:57 +0530

    dummy data for wishlist page added

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { Login } from "./Pages/Login/Login";
 import { HomePage } from "./Pages/HomePage/HomePage";
 import { SignUp } from "./Pages/SignUp/SignUp";
 import { Cart } from "./Pages/CartPage/Cart";
-
+import Wishlist from "./Pages/WishlistPage/Wishlist";
 function App() {
   return (
     <>
@@ -16,6 +16,7 @@ function App() {
         <Route path="/cart" element={<Cart />} />
         <Route path="/login" element={<Login />} />
         <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/wishlist" element={<Wishlist/>}/>
       </Routes>
       <Footer />
     </>
--- src/Pages/WishlistPage/Wishlist.js
+++ src/Pages/WishlistPage/Wishlist.js
@@ -1,22 +1,53 @@
-import React from "react";  
-function deleteItem(){
+import React, { useState } from "react";
+import photo_url1 from '../WishlistPage/assets/abc.jpg';
+import './Wishlist.css';
+function Wishlist() {
+    const [noOfItems, setNoOfItems] = useState(0);
+    const [items, setItems] = useState([
+        {
+            photo_url: '../WishlistPage/assets/abc.jpg',
+            price: 299,
+            description: 'This is the best shoes for mens'
+        },
+        {
+            photo_url :'../WishlistPage/assets/shoes.jpg',
+            price:4000,
+            description:'Bacca Bucci shoes best quality and best comfort'
+        },{
+            photo_url :'../WishlistPage/assets/shoes.jpg',
+            price:4000,
+            description:'Bacca Bucci shoes best quality and best comfort'
+        }
+    ]);
 
-}
-function Wishlist()
-{
-    var noOfItems=0;
-    return
-    {
-        <div className="mywishlist-pad10">
-            <div><h2>My WishList ({noOfItems})</h2></div>
-            <hr />
-            <div className="Itemscontainer">
-                <div className="itemimage"></div>
-                <div className="itemdetails"></div>
-                <button className="delete" onClick={deleteItem()}></button>
-            </div>
-        </div>
+    function deleteItem(index) {
+        const updatedItems = [...items];
+        updatedItems.splice(index, 1);
+        setItems(updatedItems);
+        setNoOfItems(updatedItems.length);
     }
+
+    return (
+        <>
+            <div className="mywishlist-pad10">
+                <div><h2>My WishList ({noOfItems})</h2></div>
+                <hr />
+                <div className="itemsContainer">
+                    {items.map((item, index) => (
+                        <div key={index} className="item">
+                            <div className="itemImage"><img src={photo_url1} alt="Photo not loaded" /></div>
+                            <div className="itemDetails">
+                                {item.description}
+                                <br />
+                                <div className="price">PRICE: RS{item.price}</div>
+                            </div>
+                            <button className="delete" onClick={() => deleteItem(index)}>Delete</button>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </>
+    );
 }
 
 export default Wishlist;
