a33c7fd211ca05e1522b1e4f7f20d6ef1f627fd2
Devendra-009 <devendrapatidar6611@gmail.com>
2024-05-12 18:17:00 +0530

    Added cart

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import "./App.css";
-import { Route, Routes } from "react-router-dom";
-import { Navbar } from "./Components/Navbar/Navbar";
-import { Footer } from "./Components/Footer/Footer";
-import { Login } from "./Pages/Login/Login";
-import { HomePage } from "./Pages/HomePage/HomePage";
-import { SignUp } from "./Pages/SignUp/SignUp";
-import { Cart } from "./Pages/CartPage/Cart";
+import React , {useState} from 'react';
+import Navbar from './Components/Navbar';
+import Amazon from './Components/Amazon';
+import Cart from './Components/Cart';
+import './styles/amazon.css';
 
-function App() {
+const App = () => {
+	const [show, setShow] = useState(true);
+	const [cart , setCart] = useState([]);
+	const [warning, setWarning] = useState(false);
+
+	const handleClick = (item)=>{
+		let isPresent = false;
+		cart.forEach((product)=>{
+			if (item.id === product.id)
+			isPresent = true;
+		})
+		if (isPresent){
+			setWarning(true);
+			setTimeout(()=>{
+				setWarning(false);
+			}, 2000);
+			return ;
+		}
+		setCart([...cart, item]);
+	}
+
+	const handleChange = (item, d) =>{
+		let ind = -1;
+		cart.forEach((data, index)=>{
+			if (data.id === item.id)
+				ind = index;
+		});
+		const tempArr = cart;
+		tempArr[ind].amount += d;
+		
+		if (tempArr[ind].amount === 0)
+			tempArr[ind].amount = 1;
+		setCart([...tempArr])
+	}
+
   return (
-    <>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/sign-up" element={<SignUp />} />
-      </Routes>
-      <Footer />
-    </>
-  );
+	<React.Fragment>
+		<Navbar size={cart.length} setShow={setShow} />
+		{
+			show ? <Amazon handleClick={handleClick} /> : <Cart cart={cart} setCart={setCart} handleChange={handleChange} />
+		}
+		{
+			warning && <div className='warning'>Item is already added to your cart</div>
+		}
+	</React.Fragment>
+  )
 }
 
-export default App;
+export default App
